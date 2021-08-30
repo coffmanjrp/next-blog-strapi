4241213c5a45b55a8e5446c9406a794a1212a9c8
coffmanjrp <coffmanjrp@gmail.com>
2021-08-30 00:50:57 -0700

    Create api library file

---
+++ frontend/lib/api.js
@@ -0,0 +1,14 @@
+export const getStrapiURL = (path = '') => {
+  return `${
+    process.env.NEXT_PUBLIC_STRAPI_API_URL || 'http://localhost:1337'
+  }${path}`;
+};
+
+// Helper to make GET requests to Strapi
+export const fetchApi = async (path) => {
+  const requestUrl = getStrapiURL(path);
+  const response = await fetch(requestUrl);
+  const data = await response.json();
+
+  return data;
+};
